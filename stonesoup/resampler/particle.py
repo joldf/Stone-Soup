4dae0d9e0ce045e75dd8a810f991fb628774c67e
jordi <jmbarr@dstl.gov.uk>
2021-12-15 23:11:54 +0000

    Updated some tests with StateVectors particle filter.

--- stonesoup/resampler/particle.py
+++ stonesoup/resampler/particle.py
@@ -44,8 +44,9 @@ class SystematicResampler(Resampler):
         index = weight_order[np.searchsorted(cdf, np.log(u_j))]
         new_particles = ParticleState(state_vector=particles.state_vector[:, index],
                                       weight=[weight]*n_particles,
-                                      parent=ParticleState(state_vector=particles.state_vector[:, index],
-                                                           weight=particles.weight[index],
-                                                           timestamp=particles.timestamp),
+                                      parent=ParticleState(
+                                          state_vector=particles.state_vector[:, index],
+                                          weight=particles.weight[index],
+                                          timestamp=particles.timestamp),
                                       timestamp=particles.timestamp)
         return new_particles
